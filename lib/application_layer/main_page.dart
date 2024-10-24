77a96b9003b68e18dc065d52bb194fd11dd6800d
felixmielcarek <felixmielcarek@fm-desktop-debian.home>
2024-10-25 01:30:10 +0200

    :lipstick: Add navbar and background to ranking page

--- lib/application_layer/main_page.dart
+++ lib/application_layer/main_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:genius/application_layer/genius_colors.dart';
 import 'package:genius/application_layer/ranking_page.dart';
 
 class MainPage extends StatefulWidget {
@@ -11,21 +13,60 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
 
+  final List<Widget> _children = [
+    const RankingPage(),
+    const RankingPage(),
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const PreferredSize(
           preferredSize: Size.fromHeight(80.0), child: Row()),
-      body: const RankingPage(),
+      body: _children[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Classements'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Tournois'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Acceuil'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Matchs'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Mes Pronos')
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                  'assets/icons/podium${_selectedIndex == 0 ? '_primary' : ''}.svg',
+                  height: 30),
+              label: 'Classements'),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                  'assets/icons/trophy${_selectedIndex == 1 ? '_primary' : ''}.svg',
+                  height: 30),
+              label: 'Tournois'),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                  'assets/icons/stadium${_selectedIndex == 2 ? '_primary' : ''}.svg',
+                  height: 30),
+              label: 'Acceuil'),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                  'assets/icons/field${_selectedIndex == 3 ? '_primary' : ''}.svg',
+                  height: 30),
+              label: 'Matchs'),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                  'assets/icons/ticket${_selectedIndex == 4 ? '_primary' : ''}.svg',
+                  height: 30),
+              label: 'Mes Pronos'),
         ],
+        onTap: onTabTapped,
         currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: GeniusColors.primary,
+        unselectedItemColor: Colors.black,
+        selectedLabelStyle:
+            const TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
+        unselectedLabelStyle:
+            const TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
       ),
     );
   }
