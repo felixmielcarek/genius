9689a9e187d48979bd16a6cf10c8a4c142aef5de
felixmielcarek <felixmielcarek@fm-desktop-debian.home>
2024-10-25 02:43:09 +0200

    :construction: Add app bar

--- lib/application_layer/genius_colors.dart
+++ lib/application_layer/genius_colors.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class GeniusColors {
   static const Color primary = Color(0xFF7584ff); // violet bleu
   static const Color secondary = Color(0xFFecf1ff); // bleu ciel clair
-  static const Color accent = Color(0xFFffe3ba); // jaune
+  static const Color accentStart = Color(0xFFffe3ba); // jaune
+  static const Color accentEnd = Color(0xFFEE9714); // orange
   static const Color notification = Color(0xFFD80027); // rouge
 
   static const Color textDark = Color(0xFF000000); // Black
@@ -14,8 +15,6 @@ class GeniusColors {
 /*
 #BCFFCF ⇒ vert claire
 #108236 ⇒ vert
-#FFE3BA ⇒ jaune
-#EE9714 ⇒ orange
 #FFD1D9 ⇒ rouge claire
 #D80027 ⇒ rouge
 #9EA5AD ⇒ gris
--- lib/application_layer/main_page.dart
+++ lib/application_layer/main_page.dart
@@ -11,9 +11,15 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final String picture = "person.png";
+  final int coinsWallet = 452;
+
   int _selectedIndex = 0;
 
   final List<Widget> _children = [
+    const RankingPage(),
+    const RankingPage(),
+    const RankingPage(),
     const RankingPage(),
     const RankingPage(),
   ];
@@ -26,48 +32,98 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const PreferredSize(
-          preferredSize: Size.fromHeight(80.0), child: Row()),
-      body: _children[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                  'assets/icons/podium${_selectedIndex == 0 ? '_primary' : ''}.svg',
-                  height: 30),
-              label: 'Classements'),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                  'assets/icons/trophy${_selectedIndex == 1 ? '_primary' : ''}.svg',
-                  height: 30),
-              label: 'Tournois'),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                  'assets/icons/stadium${_selectedIndex == 2 ? '_primary' : ''}.svg',
-                  height: 30),
-              label: 'Acceuil'),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                  'assets/icons/field${_selectedIndex == 3 ? '_primary' : ''}.svg',
-                  height: 30),
-              label: 'Matchs'),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                  'assets/icons/ticket${_selectedIndex == 4 ? '_primary' : ''}.svg',
-                  height: 30),
-              label: 'Mes Pronos'),
-        ],
-        onTap: onTabTapped,
-        currentIndex: _selectedIndex,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: GeniusColors.primary,
-        unselectedItemColor: Colors.black,
-        selectedLabelStyle:
-            const TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
-        unselectedLabelStyle:
-            const TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
-      ),
-    );
+    return SafeArea(
+        child: Scaffold(
+            appBar: PreferredSize(
+                preferredSize: const Size.fromHeight(80.0),
+                child: Row(children: [
+                  Container(
+                      decoration: BoxDecoration(
+                          color: GeniusColors.primary,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 3.0),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                blurRadius: 5)
+                          ]),
+                      child: ClipOval(
+                          child: Image(
+                              image: AssetImage('assets/$picture'),
+                              height: 35))),
+                  Column(children: [
+                    Row(children: [
+                      SvgPicture.asset('assets/icons/coins.svg',
+                          semanticsLabel: 'Coin icon', height: 20),
+                      Text(coinsWallet.toString())
+                    ]),
+                    const Text("Champion")
+                  ]),
+                  const Spacer(),
+                  Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Container(
+                          margin: const EdgeInsets.only(bottom: 10),
+                          height: 40,
+                          width: 135,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              gradient: const LinearGradient(
+                                  colors: [
+                                    GeniusColors.accentStart,
+                                    GeniusColors.accentEnd
+                                  ],
+                                  begin: Alignment.bottomLeft,
+                                  end: Alignment.topCenter)),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SvgPicture.asset('assets/icons/shop.svg',
+                                    height: 25),
+                                const Text('Boutique',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                        color: GeniusColors.textPrimary))
+                              ])))
+                ])),
+            body: _children[_selectedIndex],
+            bottomNavigationBar: BottomNavigationBar(
+                items: <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                      icon: SvgPicture.asset(
+                          'assets/icons/podium${_selectedIndex == 0 ? '_primary' : ''}.svg',
+                          height: 30),
+                      label: 'Classements'),
+                  BottomNavigationBarItem(
+                      icon: SvgPicture.asset(
+                          'assets/icons/trophy${_selectedIndex == 1 ? '_primary' : ''}.svg',
+                          height: 30),
+                      label: 'Tournois'),
+                  BottomNavigationBarItem(
+                      icon: SvgPicture.asset(
+                          'assets/icons/stadium${_selectedIndex == 2 ? '_primary' : ''}.svg',
+                          height: 30),
+                      label: 'Acceuil'),
+                  BottomNavigationBarItem(
+                      icon: SvgPicture.asset(
+                          'assets/icons/field${_selectedIndex == 3 ? '_primary' : ''}.svg',
+                          height: 30),
+                      label: 'Matchs'),
+                  BottomNavigationBarItem(
+                      icon: SvgPicture.asset(
+                          'assets/icons/ticket${_selectedIndex == 4 ? '_primary' : ''}.svg',
+                          height: 30),
+                      label: 'Mes Pronos')
+                ],
+                onTap: onTabTapped,
+                currentIndex: _selectedIndex,
+                type: BottomNavigationBarType.fixed,
+                selectedItemColor: GeniusColors.primary,
+                unselectedItemColor: Colors.black,
+                selectedLabelStyle:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
+                unselectedLabelStyle: const TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 10))));
   }
 }
